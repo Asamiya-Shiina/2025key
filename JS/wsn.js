f5517c2bd93e3df58bbf2c71ce4f002ff843eaab
Asamiya Shiina <Asamiya_Shiina@hotmail.com>
2025-07-04 14:46:02 +0800

    Add files via upload

--- JS/wsn.js
+++ JS/wsn.js
@@ -1,8 +1,134 @@
-var zero = document.createTextNode("test")
+var zero = document.createTextNode(" ")
+var test = document.createTextNode("test")
 var key = document.getElementById("key")
+var br = document.createElement("br")
 
 //答案于此显现！！！！！！！！！！！！！
 var q1 = document.getElementById("Q1_title")
 q1.addEventListener("click",function(){
-    key.append(zero)
+    let r_img = document.getElementById("img")
+    r_img.remove()
+    let div = document.createElement("div")
+    key.appendChild(div)
+    let id = document.createAttribute("id")
+    id.value = "img"
+    div.setAttributeNode(id)
+    let img = document.getElementById("img")
+    let img1 = document.createElement("img")
+    img1.src = "../image/key1_1.png" 
+    let img2 = document.createElement("img")
+    img2.src = "../image/key1_2.png" 
+    let img3 = document.createElement("img")
+    img3.src = "../image/key1_3.png" 
+    let img4 = document.createElement("img")
+    img4.src = "../image/key1_4.png" 
+    img.appendChild(img1)
+    img.appendChild(img2)
+    img.appendChild(img3)
+    img.appendChild(img4)
+})
+
+var q2 = document.getElementById("Q2_title")
+q2.addEventListener("click",function(){
+    let r_img = document.getElementById("img")
+    r_img.remove()
+    let div = document.createElement("div")
+    key.appendChild(div)
+    let id = document.createAttribute("id")
+    id.value = "img"
+    div.setAttributeNode(id)
+    let img = document.getElementById("img")
+    let img1 = document.createElement("img")
+    img1.src = "../image/key2_1.png" 
+    let img2 = document.createElement("img")
+    img2.src = "../image/key2_2.png" 
+    let img3 = document.createElement("img")
+    img3.src = "../image/key2_3.png" 
+    let img4 = document.createElement("img")
+    img4.src = "../image/key2_4.png" 
+    img.appendChild(img1)
+    img.appendChild(img2)
+    img.appendChild(img3)
+    img.appendChild(img4)
+})
+
+var q3 = document.getElementById("Q3_title")
+q3.addEventListener("click",function(){
+    let r_img = document.getElementById("img")
+    r_img.remove()
+    let div = document.createElement("div")
+    key.appendChild(div)
+    let id = document.createAttribute("id")
+    id.value = "img"
+    div.setAttributeNode(id)
+    let img = document.getElementById("img")
+    let img1 = document.createElement("img")
+    img1.src = "../image/key3_1.png" 
+    let img2 = document.createElement("img")
+    img2.src = "../image/key3_2.png" 
+    let img3 = document.createElement("img")
+    img3.src = "../image/key3_3.png" 
+    img.appendChild(img1)
+    img.appendChild(img2)
+    img.appendChild(img3)
+})
+
+var q4 = document.getElementById("Q4_title")
+q4.addEventListener("click",function(){
+    let r_img = document.getElementById("img")
+    r_img.remove()
+    let div = document.createElement("div")
+    key.appendChild(div)
+    let id = document.createAttribute("id")
+    id.value = "img"
+    div.setAttributeNode(id)
+    let img = document.getElementById("img")
+    let img1 = document.createElement("img")
+    img1.src = "../image/key4_1.png" 
+    let img2 = document.createElement("img")
+    img2.src = "../image/key4_2.png" 
+    let img3 = document.createElement("img")
+    img3.src = "../image/key4_3.png" 
+    img.appendChild(img1)
+    img.appendChild(img2)
+    img.appendChild(img3)
+})
+
+var q5 = document.getElementById("Q5_title")
+q5.addEventListener("click",function(){
+    let r_img = document.getElementById("img")
+    r_img.remove()
+    let div = document.createElement("div")
+    key.appendChild(div)
+    let id = document.createAttribute("id")
+    id.value = "img"
+    div.setAttributeNode(id)
+    let img = document.getElementById("img")
+    let img1 = document.createElement("img")
+    img1.src = "../image/key5_1.png" 
+    let img2 = document.createElement("img")
+    img2.src = "../image/key5_2.png" 
+    let img3 = document.createElement("img")
+    img3.src = "../image/key5_3.png" 
+    let img4 = document.createElement("img")
+    img4.src = "../image/key5_4.png" 
+    img.appendChild(img1)
+    img.appendChild(img2)
+    img.appendChild(img3)
+    img.appendChild(img4)
+})
+
+var q6 = document.getElementById("Q6_title")
+q6.addEventListener("click",function(){
+    let r_img = document.getElementById("img")
+    r_img.remove()
+    let div = document.createElement("div")
+    key.appendChild(div)
+    let id = document.createAttribute("id")
+    id.value = "img"
+    div.setAttributeNode(id)
+    let img = document.getElementById("img")
+    let img1 = document.createElement("img")
+    img1.src = "../image/key6.png" 
+    img.appendChild(img1)
 })
